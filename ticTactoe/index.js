0aa9bde8eb7be8495accd0f5895d908a3d758beb
MerikKuhn <mkuhn181@west-mec.org>
2022-11-09 18:19:07 -0700

    project stuff

--- ticTactoe/index.js
+++ ticTactoe/index.js
@@ -1,9 +1,47 @@
 let turn = "X";
-function tic1(){
-    imgswap("img1")
+function _turn(){
+    if(turn == "X"){
+        turn = 'O'
+    }else if(turn == 'O'){
+        turn = 'X'
+    }
 }
 function imgswap(a){
-    if(turn == "X"){
-        document.getElementById(a).src = "x-tictactoe.png"
-    }
+    document.getElementById(a).src = "TTT-X.png"
+}
+function tic1(){
+    imgswap('img1')
+    _turn()
+}
+function tic2(){
+    imgswap('img2')
+    _turn()
+}
+function tic3(){
+    imgswap('img3')
+    _turn()
+}
+function tic4(){
+    imgswap('img4')
+    _turn()
+}
+function tic5(){
+    imgswap('img5')
+    _turn()
+}
+function tic6(){
+    imgswap('img6')
+    _turn()
+}
+function tic7(){
+    imgswap('img7')
+    _turn()
+}
+function tic8(){
+    imgswap('img8')
+    _turn()
+}
+function tic9(){
+    imgswap('img9')
+    _turn()
 }
